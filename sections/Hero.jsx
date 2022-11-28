a0968c2e966bbc6d8dd3f7ab7cc57adff14bda83
Subham <97989643+Subham-Maity@users.noreply.github.com>
2022-11-29 02:18:47 +0530

    Globe Animation With Side Motion Add 🌍
    1. Margin add between texts (LEARNDAILY)
    
    2.  Motion div with "SlideIn" Function add
    
    export const slideIn = (direction, type, delay, duration) => ({
      hidden: {
        x: direction === 'left' ? '-100%' : direction === 'right' ? '130%' : 0,
        y: direction === 'up' ? '100%' : direction === 'down' ? '100%' : 0,
      },
      show: {
        x: 0,
        y: 0,
        transition: {
          type,
          delay,
          duration,
          ease: 'easeOut',
        },
      },
    });
    
    3. Globe Animated SVG
    
    <div className="wrapper w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative">
    
    - Wrapper is main Globe SVG class
    - Remain is for rounded corner
    
    4.  <div className="absolute w-full h-[300px] select-animate rounded-tl-[140px] z-[0] -top-[30px]" />
    - select animated is the CSS gradient class for the upper border
    
    5. Css Change Gradient Color modified +Globe SVG animation
    - gradient-01 class change to animated only remain color same
    
    6. Hero Gradient Color usability Remove
    
    7. Motion Js right Corner Shifted to 100 to 30% more (changeable) 📝
    
    8. Add Planate.js for planate svg with css Animation

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -22,11 +22,92 @@ const Hero = () => (
                         variants={textVariant(1.2)}
                         className="flex flex-row justify-center items-center"
                     >
+                        <div className="mb-1">
                         <h1 className={styles.heroHeading}>Learn</h1>
+                        </div><div className="mb-1">
                         <div className={styles.heroDText} />
+                         </div> <div className="mb-1">
                         <h1 className={styles.heroHeading}>aily</h1>
+                    </div>
+
+
+
                     </motion.div>
                 </div>
+                <div>
+                    <motion.div
+                        variants={slideIn('right', 'tween', 0.1, 1)}
+                        className="relative w-full md:-mt-[20px] -mt-[12px]"
+                    >
+                        <div className="absolute w-full h-[300px] select-animate rounded-tl-[140px] z-[0] -top-[30px]" />
+
+                        <div className="wrapper w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative">
+                            <div className="wrapper__absolute--full beacon-wrapper beacon-wrapper-01">
+                                <div className="beacon beacon-01"></div>
+                            </div>
+                            <div className="wrapper__absolute--full beacon-wrapper beacon-wrapper-02">
+                                <div className="beacon beacon-02"></div>
+                            </div>
+                            <div className="wrapper__absolute--full beacon-wrapper beacon-wrapper-03">
+                                <div className="beacon beacon-03"></div>
+                            </div>
+
+                            <div id="particles-js" className="wrapper__absolute--full"></div>
+
+                            <div className="space-nebula__wrapper wrapper__absolute--full">
+                                <div className="space-nebula space-nebula-01"></div>
+                                <div className="space-nebula space-nebula-02"></div>
+                                <div className="space-nebula space-nebula-03"></div>
+                            </div>
+
+                            <div className="orb__wrapper">
+                                <div className="orb__mask">
+                                    <div className="orb__texture"></div>
+
+                                    <div className="orb__cloudgroup orb__cloudgroup-01">
+                                        <div className="orb__clouds orb__clouds-01"></div>
+                                        <div className="orb__clouds orb__clouds-02"></div>
+                                    </div>
+                                    <div className="orb__cloudgroup orb__cloudgroup-02">
+                                        <div className="orb__clouds orb__clouds-01"></div>
+                                        <div className="orb__clouds orb__clouds-02"></div>
+                                    </div>
+                                    <div className="orb__cloudgroup orb__cloudgroup-03">
+                                        <div className="orb__clouds orb__clouds-01"></div>
+                                        <div className="orb__clouds orb__clouds-02"></div>
+                                    </div>
+                                    <div className="orb__cloudgroup orb__cloudgroup-04">
+                                        <div className="orb__clouds orb__clouds-01"></div>
+                                        <div className="orb__clouds orb__clouds-02"></div>
+                                    </div>
+
+                                    <div className="orb__shading"></div>
+                                    <div className="orb__lighting"></div>
+                                </div>
+                            </div>
+
+                            <div className="wrapper__absolute--full landscape-clouds__wrapper">
+                                <div className="landscape-clouds landscape-clouds-01"></div>
+                                <div className="landscape-clouds landscape-clouds-02"></div>
+                            </div>
+
+                            <div className="landscape">
+                                <div className="landscape__bg landscape__bg--03"></div>
+                                <div className="landscape__bg landscape__bg--02"></div>
+                                <div className="landscape__bg landscape__bg--01"></div>
+                            </div>
+                        </div>
+
+
+
+
+
+                    </motion.div>
+
+
+                </div>
+
+
             </motion.div>
   </section>
 );
