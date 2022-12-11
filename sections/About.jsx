ff87e051ff81e413fc2793862653f29c73ebd10f
Subham <97989643+Subham-Maity@users.noreply.github.com>
2022-12-12 02:13:36 +0530

    About -> Typing Text Function inside motion div 😊
    1. Typing Text Function with title "| About CodeXam"
    2. textStyles="text-center" (alignment)
    3. Motion div property -> Variants {staggerContainer}
    
    export const staggerContainer = (staggerChildren, delayChildren) => ({
      hidden: {},
      show: {
        transition: {
          staggerChildren,
          delayChildren,
        },
      },
    });
    
    initial="hidden" -> it's mean that the element is hidden
     whileInView="show" ->This is the magic line that makes the animation work
    
     viewport={{ once: false, amount: 0.25 }} -> 25% of the viewport height -> same like hero
    
    className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`} ->
    
    - mx-auto ->mx-auto class for horizontally centering fixed-width block level content
    
    -
    
    `index.js`
    - innerWidth (custom class) ->   innerWidth: '2xl:max-w-[1280px] w-full',
    
    - flexCenter (custom class) ->    flexCenter: 'flex justify-center items-center',
    
    - flex-col-> https://tailwindcss.com/docs/flex-direction#column

--- sections/About.jsx
+++ sections/About.jsx
@@ -9,6 +9,15 @@ import { fadeIn, staggerContainer } from '../utils/motion';
 const About = () => (
     <section className={`${styles.paddings} relative z-10`}>
         <div className="gradient-02 z-0" />
+        <motion.div
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}
+            className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+        >
+            <TypingText title="| About CodeXam" textStyles="text-center" />
+        </motion.div>
         About section
   </section>
 );
