1a8e918c880559863923b203f8b0b9e329bab378
Subham <97989643+Subham-Maity@users.noreply.github.com>
2022-12-12 14:46:33 +0530

    Description About CodeXam With Motion Paragraph 📒
    1.  motion.p -> text inside the paragraph
    
    • variants={fadeIn('up', 'tween', 0.2, 1)}
    
    ° Direction -> direction up means from bottom to top
    ° Tween  ->  Set type to "tween" to use a duration-based animation. If any non-orchestration transition values are set without a type property, this is used as the default animation.
    ○ https://www.framer.com/docs/transition/#tween
    ° 0.2 ->  0.2 is the delay
    ° 1 is the duration of the animation
    
    2. className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
    
    • mt is margin top sm:text is for small screens and text is for large screens text-center is for centering the text and text-secondary-white is for the color of the text is white with half opacity
    
    3.  span className="font-extrabold text-white" -> font extra bold text color white
    
    4. {' '} -> some leading or trailing spaces

--- sections/About.jsx
+++ sections/About.jsx
@@ -18,6 +18,19 @@ const About = () => (
         >
             <TypingText title="| About CodeXam" textStyles="text-center" />
         </motion.div>
+        <motion.p
+            variants={fadeIn('up', 'tween', 0.2, 1)}
+            className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+        >
+            <span className="font-extrabold text-white">CodeXam</span> is a learning platform that helps individuals learn about coding and programming.{' '}
+            <span className="font-extrabold text-white">
+          The platform offers a range of courses and tutorials, allowing users to build their skills and knowledge in a
+        </span>{' '}
+            variety of programming languages.{' '}
+            <span className="font-extrabold text-white">Whether</span> you're a beginner or an experienced programmer, Codexam has something to offer.{' '}
+            <span className="font-extrabold text-white">explore</span> the world
+            of the CodeXam by scrolling down
+        </motion.p>
         About section
   </section>
 );
